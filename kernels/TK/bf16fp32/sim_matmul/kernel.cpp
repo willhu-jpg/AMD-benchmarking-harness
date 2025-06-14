27f12ef60b4d8e14b4efa6840a08a3065b0a832f
Simran Arora <emailsimran@gmail.com>
2025-06-14 09:39:42 -0500

    cleaned kernel

--- kernels/TK/bf16fp32/sim_matmul/kernel.cpp
+++ kernels/TK/bf16fp32/sim_matmul/kernel.cpp
@@ -30,7 +30,7 @@ struct micro_globals {
     size_t dynamic_shared_memory() { return 16384*2; }
 };
 
-__global__ __launch_bounds__(NUM_THREADS, 2)
+__global__ __launch_bounds__(NUM_THREADS, 1)
 void micro_tk(const micro_globals g) {
 
     extern __shared__ alignment_dummy __shm[];
@@ -43,16 +43,17 @@ void micro_tk(const micro_globals g) {
     for (int i = 0; i < 4; i++) {
         zero(C_accum[i]);
     }
-    rt_bf<REG_BLOCK, K_STEP> a_reg;
-    rt_bf<K_STEP, REG_BLOCK, ducks::rt_layout::col> b_reg;
+
+    rt_bf<REG_BLOCK, K_STEP> a_reg_0, a_reg_1;
+    rt_bf<K_STEP, REG_BLOCK, ducks::rt_layout::col> b_reg_0, b_reg_1;
 
 
     int row = blockIdx.y;
     int col = blockIdx.x;
 
     int warp_id = kittens::warpid();
-    int warp_row = warp_id / 2;  // Keep original logic
-    int warp_col = warp_id % 2;  // Keep original logic
+    int warp_row = warp_id / 2;
+    int warp_col = warp_id % 2;
 
     int num_tiles = K / K_STEP;
     for (int tile = 0; tile < num_tiles; ++tile) {
@@ -61,21 +62,20 @@ void micro_tk(const micro_globals g) {
         G::load(Bs, g.b, {0, 0, tile, col});
         __syncthreads();
 
-        #pragma unroll
-        for (int i = 0; i < 4; i++) {
+        int warp_id   = kittens::warpid();
+        int warp_row  = warp_id / 2;
+        int warp_col  = warp_id % 2;
 
-            int offset_row = i / 2;  // 0,0,1,1
-            int offset_col = i % 2;  // 0,1,0,1
+        load(a_reg_0, subtile_inplace<REG_BLOCK, K_STEP>(As, {warp_row + 0, 0}));
+        load(a_reg_1, subtile_inplace<REG_BLOCK, K_STEP>(As, {warp_row + 2, 0}));
+        load(b_reg_0, subtile_inplace<K_STEP, REG_BLOCK>(Bs, {0, warp_col + 0}));
+        load(b_reg_1, subtile_inplace<K_STEP, REG_BLOCK>(Bs, {0, warp_col + 2}));
 
-            int subtile_row = offset_row * 2 + warp_row;  
-            int subtile_col = offset_col * 2 + warp_col;
+        mma_AB(C_accum[0], a_reg_0, b_reg_0, C_accum[0]);
+        mma_AB(C_accum[1], a_reg_0, b_reg_1, C_accum[1]);
+        mma_AB(C_accum[2], a_reg_1, b_reg_0, C_accum[2]);
+        mma_AB(C_accum[3], a_reg_1, b_reg_1, C_accum[3]);
 
-            auto st_sub_a = subtile_inplace<REG_BLOCK, K_STEP>(As, {subtile_row, 0});
-            auto st_sub_b = subtile_inplace<K_STEP, REG_BLOCK>(Bs, {0, subtile_col});
-            load(a_reg, st_sub_a);
-            load(b_reg, st_sub_b);
-            mma_AB(C_accum[i], a_reg, b_reg, C_accum[i]);
-        }
     }
 
     #pragma unroll
