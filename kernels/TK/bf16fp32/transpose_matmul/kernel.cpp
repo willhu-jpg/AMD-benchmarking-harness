9c33ed6ca13a67e7a29abfc2fe73bda2ceff2d7a
Simran Arora <emailsimran@gmail.com>
2025-06-16 01:58:09 -0500

    save clean

--- kernels/TK/bf16fp32/transpose_matmul/kernel.cpp
+++ kernels/TK/bf16fp32/transpose_matmul/kernel.cpp
@@ -26,7 +26,7 @@ struct micro_globals {
 
     dim3 grid()  { return dim3(N / BLOCK_SIZE, M / BLOCK_SIZE); } 
     dim3 block() { return dim3(NUM_THREADS); } 
-    size_t dynamic_shared_memory() { return 16384*2; }
+    size_t dynamic_shared_memory() { return 32768; }
 };
 
 __global__ __launch_bounds__(NUM_THREADS, 1)
@@ -36,9 +36,10 @@ void micro_tk(const micro_globals g) {
     shared_allocator al((int*)&__shm[0]);
     st_bf<BLOCK_SIZE, K_STEP> (&As) = al.allocate<st_bf<BLOCK_SIZE, K_STEP>>();
     st_bf<BLOCK_SIZE, K_STEP> (&Bs) = al.allocate<st_bf<BLOCK_SIZE, K_STEP>>();
-    (void)__shm[0];
 
     rt_fl<REG_BLOCK, REG_BLOCK, ducks::rt_layout::col> C_accum[4];
+    rt_fl<REG_BLOCK, REG_BLOCK> C_accum_row;
+    #pragma unroll
     for (int i = 0; i < 4; i++) { zero(C_accum[i]); }
 
     rt_bf<REG_BLOCK, K_STEP> a_reg_0, a_reg_1, b_reg_0, b_reg_1;
@@ -51,24 +52,21 @@ void micro_tk(const micro_globals g) {
     int warp_col = warp_id % 2;
 
     int num_tiles = K / K_STEP;
+    #pragma unroll
     for (int tile = 0; tile < num_tiles; ++tile) {
 
         G::load(As, g.a, {0, 0, row, tile});
         G::load(Bs, g.b, {0, 0, col, tile});
         __syncthreads();
 
-        int warp_id   = kittens::warpid();
-        int warp_row  = warp_id / 2;
-        int warp_col  = warp_id % 2;
-
-        load(a_reg_0, subtile_inplace<REG_BLOCK, K_STEP>(As, {warp_row + 0, 0}));
+        load(a_reg_0, subtile_inplace<REG_BLOCK, K_STEP>(As, {warp_row, 0}));
+        load(b_reg_0, subtile_inplace<REG_BLOCK, K_STEP>(Bs, {warp_col, 0}));
         load(a_reg_1, subtile_inplace<REG_BLOCK, K_STEP>(As, {warp_row + 2, 0}));
-        load(b_reg_0, subtile_inplace<REG_BLOCK, K_STEP>(Bs, {warp_col + 0, 0}));
         load(b_reg_1, subtile_inplace<REG_BLOCK, K_STEP>(Bs, {warp_col + 2, 0}));
 
         mma_ABt(C_accum[0], a_reg_0, b_reg_0, C_accum[0]);
-        mma_ABt(C_accum[1], a_reg_0, b_reg_1, C_accum[1]);
         mma_ABt(C_accum[2], a_reg_1, b_reg_0, C_accum[2]);
+        mma_ABt(C_accum[1], a_reg_0, b_reg_1, C_accum[1]);
         mma_ABt(C_accum[3], a_reg_1, b_reg_1, C_accum[3]);
     }
 
@@ -79,7 +77,9 @@ void micro_tk(const micro_globals g) {
         
         int global_row = row * 4 + offset_row * 2 + warp_row;  
         int global_col = col * 4 + offset_col * 2 + warp_col;
-        store(g.c, C_accum[i], {0, 0, global_row, global_col});
+
+        swap_layout(C_accum_row, C_accum[i]);
+        store(g.c, C_accum_row, {0, 0, global_row, global_col});
     }
 }
 
@@ -100,6 +100,4 @@ PYBIND11_MODULE(tk_kernel, m) {
     py::bind_kernel<micro_tk>(m, "micro_tk", &micro_globals::a, &micro_globals::b, &micro_globals::c); 
     py::bind_function<dispatch_micro>(m, "dispatch_micro", &micro_globals::a, &micro_globals::b, &micro_globals::c);
 }
-
-
 
