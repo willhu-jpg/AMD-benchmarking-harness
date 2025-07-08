44e1a8c65e9b5eded974e93abf666d6ace9d0ed7
Simran Arora <emailsimran@gmail.com>
2025-07-07 23:23:36 -0500

    update

--- kernels/TK/bf16fp32/mi325x/256_128_64_32_outbf16/256_128_64_32.cpp
+++ kernels/TK/bf16fp32/mi325x/256_128_64_32_outbf16/256_128_64_32.cpp
@@ -31,45 +31,11 @@ struct micro_globals {
     _gl_B b;
     _gl_C c;
 
-    dim3 grid()  { return dim3(304, 1); } ;
+    // dim3 grid()  { return dim3(304, 1); } ;
+    dim3 grid()  { return dim3(N / BLOCK_SIZE_N, M / BLOCK_SIZE_M); } 
     dim3 block() { return dim3(NUM_THREADS); } 
-    size_t dynamic_shared_memory() { return 65536; } //49152; }
+    size_t dynamic_shared_memory() { return 49152; } //49152; }
 };
-
-
-__device__ inline int2 get_task_idx(int task_id) {
-    int wgid = task_id; 
-
-    const int total_tiles = (M / BLOCK_SIZE_M) * (N / BLOCK_SIZE_N);
-    if (task_id >= total_tiles) return {-1, -1};  // signal done
-
-    const int NUM_WGS = (N / BLOCK_SIZE_N) * (M / BLOCK_SIZE_M);
-    const int NUM_XCDS = 8;
-    const int CUS_PER_XCD = 38;
-    const int NUM_CUS = CUS_PER_XCD * NUM_XCDS;
-
-    // Swizzle chiplet so that wgids are in the same XCD.
-    wgid = (wgid % NUM_XCDS) * (NUM_WGS / NUM_XCDS) + (wgid / NUM_XCDS);
-
-    // Swizzle for better L2 within the same XCD.
-    const int WGM = 16;
-    const int num_pid_m = ceil_div(M, BLOCK_SIZE_M);
-    const int num_pid_n = ceil_div(N, BLOCK_SIZE_N);
-
-    int num_wgid_in_group = WGM * num_pid_n;
-    int group_id = wgid / num_wgid_in_group;
-    int first_pid_m = group_id * WGM;
-    int group_size_m = min(num_pid_m - first_pid_m, WGM);
-    int pid_m = first_pid_m + ((wgid % num_wgid_in_group) % group_size_m);
-    int pid_n = (wgid % num_wgid_in_group) / group_size_m;
-
-    // Assign the tile's row/column based on the pid_m and pid_n.
-    const int row = pid_m; // blockIdx.x
-    const int col = pid_n; // blockIdx.y
-
-    return {pid_m, pid_n};
-}
-
 
 __global__ __launch_bounds__(NUM_THREADS, 2)
 void micro_tk(const micro_globals g) {
@@ -84,111 +50,125 @@ void micro_tk(const micro_globals g) {
     const int num_tiles = K / K_STEP;
 
 
-    for(int task_iter = 0; true; task_iter++) {
+    // L2 Start
+    int wgid = (blockIdx.y * gridDim.x) + blockIdx.x;
+    const int NUM_WGS = gridDim.x * gridDim.y;
+    const int NUM_XCDS = 8;
+    const int CUS_PER_XCD = 38;
+    const int NUM_CUS = CUS_PER_XCD * NUM_XCDS;
+    // Swizzle chiplet so that wgids are in the same XCD.
+    wgid = (wgid % NUM_XCDS) * (NUM_WGS / NUM_XCDS) + (wgid / NUM_XCDS);
+    // Swizzle for better L2 within the same XCD.
+    const int WGM = 16;
+    const int num_pid_m = ceil_div(M, BLOCK_SIZE_M);
+    const int num_pid_n = ceil_div(N, BLOCK_SIZE_N);
+    int num_wgid_in_group = WGM * num_pid_n;
+    int group_id = wgid / num_wgid_in_group;
+    int first_pid_m = group_id * WGM;
+    int group_size_m = min(num_pid_m - first_pid_m, WGM);
+    int pid_m = first_pid_m + ((wgid % num_wgid_in_group) % group_size_m);
+    int pid_n = (wgid % num_wgid_in_group) / group_size_m;
+    // Assign the tile's row/column based on the pid_m and pid_n.
+    const int row = pid_m; 
+    const int col = pid_n;
+    // L2 end
 
-        int task_id = blockIdx.x + task_iter * gridDim.x;
-        int2 rowcol = get_task_idx(task_id);
-        if(rowcol.x == -1) { break; }
-        const int row = rowcol.x;
-        const int col = rowcol.y;
+    rt_bf<REG_BLOCK, DOT_SLICE> a_reg_0[2], a_reg_1[2];
+    rt_bf<REG_BLOCK, DOT_SLICE> b_reg_0[2], b_reg_1[2];
+    rt_fl<REG_BLOCK, REG_BLOCK, ducks::rt_layout::col> C_accum[4];
+    for (int i = 0; i < 4; i++) { zero(C_accum[i]); }
 
-        rt_bf<REG_BLOCK, DOT_SLICE> a_reg_0[2], a_reg_1[2];
-        rt_bf<REG_BLOCK, DOT_SLICE> b_reg_0[2], b_reg_1[2];
-        rt_fl<REG_BLOCK, REG_BLOCK, ducks::rt_layout::col> C_accum[4];
-        for (int i = 0; i < 4; i++) { zero(C_accum[i]); }
+    // Small register buffers for pipelining
+    constexpr int BUFFER_SIZE = 128;
+    float4 a_buffer_next[BUFFER_SIZE];
+    float4 b_buffer_next[BUFFER_SIZE];
 
-        // Small register buffers for pipelining
-        constexpr int BUFFER_SIZE = 128;
-        float4 a_buffer_next[BUFFER_SIZE];
-        float4 b_buffer_next[BUFFER_SIZE];
-
-        // Load first tile into shared memory
-        G::load(As, g.a, {0, 0, row, 0});
-        G::load(Bs, g.b, {0, 0, col, 0});
+    // Load first tile into shared memory
+    G::load(As, g.a, {0, 0, row, 0});
+    G::load(Bs, g.b, {0, 0, col, 0});
+    __builtin_amdgcn_s_barrier();
+    if (warp_row == 1) {
         __builtin_amdgcn_s_barrier();
+    }
 
-        if (warp_row == 1) {
-            __builtin_amdgcn_s_barrier();
-        }
 
-        for (int tile = 0; tile < num_tiles; ++tile) {
+    for (int tile = 0; tile < num_tiles; ++tile) {
 
-            const bool loading = tile + 1 < num_tiles;
-            // Start loading NEXT data to registers
-            if (loading) {
-                load_global_to_registers<2, false, st_bf<BLOCK_SIZE_M, K_STEP>, _gl_A, coord<st_bf<BLOCK_SIZE_M, K_STEP>>, NUM_THREADS>(
-                    a_buffer_next, BUFFER_SIZE, g.a, {0, 0, row, tile + 1}, As);
-            }
-            __builtin_amdgcn_sched_barrier(0);
-
-            load_async_shared_to_register(b_reg_0[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col, 0}));
-            load_async_shared_to_register(b_reg_1[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col + 4, 0}));
-            load_async_shared_to_register(a_reg_0[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row, 0}));
-            load_async_shared_to_register(a_reg_1[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row + 2, 0}));
-            __builtin_amdgcn_sched_barrier(0);
-
-            if (loading) {
-                load_global_to_registers<2, false, st_bf<BLOCK_SIZE_N, K_STEP>, _gl_B, coord<st_bf<BLOCK_SIZE_N, K_STEP>>, NUM_THREADS>(
-                    b_buffer_next, BUFFER_SIZE, g.b, {0, 0, col, tile + 1}, Bs);
-            }
-            __builtin_amdgcn_sched_barrier(0);
-
-            load_async_shared_to_register(b_reg_0[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col, 1}));
-            load_async_shared_to_register(b_reg_1[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col + 4, 1}));
-            load_async_shared_to_register(a_reg_0[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row, 1}));
-            load_async_shared_to_register(a_reg_1[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row + 2, 1}));
-            __builtin_amdgcn_s_barrier();
-            __builtin_amdgcn_sched_barrier(0);
-
-            __builtin_amdgcn_s_setprio(1);
-            mma_ABt(C_accum[0], a_reg_0[0], b_reg_0[0], C_accum[0]);
-            mma_ABt(C_accum[1], a_reg_0[0], b_reg_1[0], C_accum[1]);
-            mma_ABt(C_accum[2], a_reg_1[0], b_reg_0[0], C_accum[2]);
-            mma_ABt(C_accum[3], a_reg_1[0], b_reg_1[0], C_accum[3]);
-            __builtin_amdgcn_s_setprio(0);
-            __builtin_amdgcn_s_barrier();
-            __builtin_amdgcn_sched_barrier(0);
-
-            // Now wait for loads and write to shared memory
-            if (loading) {
-                asm volatile("s_waitcnt vmcnt(0)");
-                store_registers_to_shared<st_bf<BLOCK_SIZE_M, K_STEP>, NUM_THREADS>(
-                    a_buffer_next, As);
-                store_registers_to_shared<st_bf<BLOCK_SIZE_N, K_STEP>, NUM_THREADS>(
-                    b_buffer_next, Bs);
-            }
-            __builtin_amdgcn_s_barrier();
-            __builtin_amdgcn_sched_barrier(0);
-
-            __builtin_amdgcn_s_setprio(1);
-            mma_ABt(C_accum[0], a_reg_0[1], b_reg_0[1], C_accum[0]);
-            mma_ABt(C_accum[1], a_reg_0[1], b_reg_1[1], C_accum[1]);
-            mma_ABt(C_accum[2], a_reg_1[1], b_reg_0[1], C_accum[2]);
-            mma_ABt(C_accum[3], a_reg_1[1], b_reg_1[1], C_accum[3]);
-            __builtin_amdgcn_s_setprio(0);
-            __builtin_amdgcn_s_barrier();
+        const bool loading = tile + 1 < num_tiles;
+        // Start loading NEXT data to registers
+        if (loading) {
+            load_global_to_registers<2, false, st_bf<BLOCK_SIZE_M, K_STEP>, _gl_A, coord<st_bf<BLOCK_SIZE_M, K_STEP>>, NUM_THREADS>(
+                a_buffer_next, BUFFER_SIZE, g.a, {0, 0, row, tile + 1}, As);
             __builtin_amdgcn_sched_barrier(0);
         }
+        
 
-        if (warp_row == 0) {
-            __builtin_amdgcn_s_barrier();
+        load_async_shared_to_register(b_reg_0[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col, 0}));
+        load_async_shared_to_register(b_reg_1[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col + 4, 0}));
+        load_async_shared_to_register(a_reg_0[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row, 0}));
+        load_async_shared_to_register(a_reg_1[0], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row + 2, 0}));
+
+        if (loading) {
+            load_global_to_registers<2, false, st_bf<BLOCK_SIZE_N, K_STEP>, _gl_B, coord<st_bf<BLOCK_SIZE_N, K_STEP>>, NUM_THREADS>(
+                b_buffer_next, BUFFER_SIZE, g.b, {0, 0, col, tile + 1}, Bs);
+            __builtin_amdgcn_sched_barrier(0);
         }
+        
 
+        load_async_shared_to_register(b_reg_0[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col, 1}));
+        load_async_shared_to_register(b_reg_1[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(Bs, {warp_col + 4, 1}));
+        load_async_shared_to_register(a_reg_0[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row, 1}));
+        load_async_shared_to_register(a_reg_1[1], subtile_inplace<REG_BLOCK, DOT_SLICE>(As, {warp_row + 2, 1}));
+        __builtin_amdgcn_s_barrier();
+        __builtin_amdgcn_sched_barrier(0);
 
-        rt_bf<REG_BLOCK, REG_BLOCK, ducks::rt_layout::col> C_accum_bf16[4];
-        #pragma unroll
-        for (int i = 0; i < 4; ++i) {
-            const int row_offset = (i / 2) * 2;
-            const int col_offset = (i % 2) * 4;
-            
-            copy(C_accum_bf16[i], C_accum[i]);
-            store(g.c, C_accum_bf16[i], {
-                0, 0,
-                row * 4 + warp_row + row_offset,
-                col * 8 + warp_col + col_offset
-            });
+        __builtin_amdgcn_s_setprio(1);
+        mma_ABt(C_accum[0], a_reg_0[0], b_reg_0[0], C_accum[0]);
+        mma_ABt(C_accum[1], a_reg_0[0], b_reg_1[0], C_accum[1]);
+        mma_ABt(C_accum[2], a_reg_1[0], b_reg_0[0], C_accum[2]);
+        mma_ABt(C_accum[3], a_reg_1[0], b_reg_1[0], C_accum[3]);
+        __builtin_amdgcn_s_setprio(0);
+        __builtin_amdgcn_s_barrier();
+        __builtin_amdgcn_sched_barrier(0);
+
+        // Now wait for loads and write to shared memory
+        if (loading) {
+            asm volatile("s_waitcnt vmcnt(0)");
+            store_registers_to_shared<st_bf<BLOCK_SIZE_M, K_STEP>, NUM_THREADS>(
+                a_buffer_next, As);
+            store_registers_to_shared<st_bf<BLOCK_SIZE_N, K_STEP>, NUM_THREADS>(
+                b_buffer_next, Bs);
         }
+        __builtin_amdgcn_s_barrier();
+        __builtin_amdgcn_sched_barrier(0);
 
+        __builtin_amdgcn_s_setprio(1);
+        mma_ABt(C_accum[0], a_reg_0[1], b_reg_0[1], C_accum[0]);
+        mma_ABt(C_accum[1], a_reg_0[1], b_reg_1[1], C_accum[1]);
+        mma_ABt(C_accum[2], a_reg_1[1], b_reg_0[1], C_accum[2]);
+        mma_ABt(C_accum[3], a_reg_1[1], b_reg_1[1], C_accum[3]);
+        __builtin_amdgcn_s_setprio(0);
+        __builtin_amdgcn_s_barrier();
+        __builtin_amdgcn_sched_barrier(0);
+    }
+
+    if (warp_row == 0) {
+        __builtin_amdgcn_s_barrier();
+    }
+
+
+    rt_bf<REG_BLOCK, REG_BLOCK, ducks::rt_layout::col> C_accum_bf16[4];
+    #pragma unroll
+    for (int i = 0; i < 4; ++i) {
+        const int row_offset = (i / 2) * 2;
+        const int col_offset = (i % 2) * 4;
+        
+        copy(C_accum_bf16[i], C_accum[i]);
+        store(g.c, C_accum_bf16[i], {
+            0, 0,
+            row * 4 + warp_row + row_offset,
+            col * 8 + warp_col + col_offset
+        });
     }
 }
 
